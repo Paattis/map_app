5085091f9c82bf4f276f5e6308474a26a55cd1c4
Paattis <leevi.paattilammi@gmail.com>
2023-10-10 19:43:23 +0300

    Add postgres connectivity to django project, add postgres service to docker-compose spec

--- mapbackend/mapbackend/settings.py
+++ mapbackend/mapbackend/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'mapbackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("DJANGO_SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("POSTGRES_DB", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
